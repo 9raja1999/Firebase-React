d483447ba5714c994eced683620ec6e6d0397a18
9raja1999 <draj29209@gmail.com>
2024-02-19 00:06:33 -0800

    testing deployment

--- src/main.jsx
+++ src/main.jsx
@@ -13,10 +13,11 @@ import store from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}>
       <ThemeProvider>
         <Navigation />
       </ThemeProvider>
-    </Provider>
+    </Provider> */}
+    <h1>Hi there</h1>
   </React.StrictMode>,
 )
